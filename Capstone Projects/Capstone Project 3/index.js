c4b0edbf17f76ce9d28be46b26e369e4f40d8436
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-07-09 16:45:21 +0700

    Started Capstone Project 3

--- Capstone Projects/Capstone Project 3/index.js
+++ Capstone Projects/Capstone Project 3/index.js
@@ -3,7 +3,29 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(express.json({extended: true}));
+let blogs = [];
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {blogs: blogs});
+})
+
+app.post("/submit", (req, res) => {
+    let newPost = {
+        blogTitle: req.body.blogTitle,
+        blogContent: req.body.blogContent
+    }
+    // Inserts at beginning instead of end
+    blogs.unshift(newPost);
+    res.redirect("/");
+})
+
+app.post("/delete", (req, res) => {
+    blogs.splice(parseInt(req.body.index), 1);
+    res.redirect("/");
+})
 
 app.listen(port, () => {
     console.log("Server started at port " + port);
