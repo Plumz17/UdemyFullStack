3623d54919ab412ef3a4876285a229e728f3c0f4
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-07-09 17:30:16 +0700

    Finished Capstone Project 3

--- Capstone Projects/Capstone Project 3/index.js
+++ Capstone Projects/Capstone Project 3/index.js
@@ -4,12 +4,19 @@ const app = express();
 const port = 3000;
 
 let blogs = [];
+let currBlog = {blogTitle: "", blogContent: ""};
 
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {blogs: blogs});
+    res.render("index.ejs", {
+        blogs: blogs,
+        currentBlogTitle: currBlog.blogTitle,
+        currentBlogContent: currBlog.blogContent,
+        currentBlogIndex: currBlog.index
+    });
+    currBlog = {blogTitle: "", blogContent: ""};
 })
 
 app.post("/submit", (req, res) => {
@@ -24,6 +31,23 @@ app.post("/submit", (req, res) => {
 
 app.post("/delete", (req, res) => {
     blogs.splice(parseInt(req.body.index), 1);
+    res.redirect("/");
+})
+
+app.post("/edit", (req, res) => {
+    const index = parseInt(req.body.index);
+    currBlog = {
+        blogTitle: blogs[index].blogTitle,
+        blogContent: blogs[index].blogContent,
+        index: index
+    };
+    res.redirect("/");
+})
+
+app.post("/update", (req, res) => {
+    const index = parseInt(req.body.index);
+    blogs[index].blogTitle = req.body.blogTitle;
+    blogs[index].blogContent = req.body.blogContent;
     res.redirect("/");
 })
 
